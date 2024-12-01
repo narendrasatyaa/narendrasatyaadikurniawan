4b86008e552bb704e23a4f2d2d7183f172c5ee33
Narendra Satya Adikurniawan <narendrasatyaadi7@gmail.com>
2024-12-01 19:59:15 +0700

    First commit

---
+++ animasi.js
@@ -0,0 +1,15 @@
+{
+    /* < script >
+        document.querySelectorAll('.nav-link').forEach(link => {
+            link.addEventListener('click', () => {
+                // Tambahkan animasi klik
+                link.classList.add('clicked');
+
+                // Hapus animasi setelah selesai (300ms sesuai durasi animasi CSS)
+                setTimeout(() => {
+                    link.classList.remove('clicked');
+                }, 300);
+            });
+        }); 
+        </script> */
+}
